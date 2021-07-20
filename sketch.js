fc82937a0905d83c7ff38018db528ea7adf09da3
Christopher Ryan Limoa <clim0061@student.monash.edu>
2021-07-20 21:18:22 +1000

    Create cells object representation

--- sketch.js
+++ sketch.js
@@ -1,7 +1,48 @@
+let cols,rows;
+const w = 40;
+
+let cells = [];
+
 function setup(){
     createCanvas(400,400);
+
+    cols = floor(width/w);
+    rows = floor(height/w);
+
+    for(let i=0;i<cols;i++){
+        for(let j=0;j<rows;j++){
+            cells.push(new Cell(i,j))
+        }
+    }
 }
 
 function draw(){
     background(51);
+
+    for(let i=0; i< cells.length; i++){
+        cells[i].draw();
+    }
+
 }
+
+class Cell {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    draw(){
+        let x = this.x * w;
+        let y = this.y * w;
+        
+        stroke(255)
+        // top line of rect
+        line(x, y, x+w, y)
+        // right line of rect
+        line(x+w, y, x+w, y+w)
+        // bottom line of rect
+        line(x+w, y+w, x, y+w)
+        // left line of rect
+        line(x, y+w, x, y)
+    }
+  }
